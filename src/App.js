a31b898bdfcc56819ea39c2e13c47aabdc6d6730
HiroshiKawano <39044771+HiroshiKawano@users.noreply.github.com>
2023-02-04 00:36:45 +0900

    Study20230203

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
+import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
-
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/T-0EW-SEbsE",
-  email: "12345@example.com",
-  phone: "090-111-2222",
-  company: {
-    name: "テスト株式会社"
-  },
-  website: "https://google.com"
-};
+import { RecoilRoot } from "recoil";
 
 export default function App() {
-  return <Router />;
+  return (
+    <RecoilRoot>
+      <UserProvider>
+        <Router />
+      </UserProvider>
+    </RecoilRoot>
+  );
 }
